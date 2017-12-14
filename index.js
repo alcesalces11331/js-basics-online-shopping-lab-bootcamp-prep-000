68550e8372e3937f2cd526298fc60ca2368b9663
Rollin Metzger <rollin.metzger@gmail.com>
2017-12-14 18:11:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cart.length > 0) {
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
     
   }
 }
